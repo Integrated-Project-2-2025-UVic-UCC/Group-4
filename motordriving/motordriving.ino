6f6966a207b53998d3787ba13becee254656aa58
Woetoe05 <wout.vanderzanden@uvic.cat>
2025-04-08 19:45:21 +0200

    bug fixed and fork added

--- motordriving/motordriving.ino
+++ motordriving/motordriving.ino
@@ -1,5 +1,11 @@
 #include <SoftwareSerial.h>
 
+// Include the AccelStepper Library//https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
+// https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
+#include <AccelStepper.h>
+
+// Define step constant
+#define MotorInterfaceType 4
 
 SoftwareSerial BTSerial(2,3); // RX | TX // or (10,11)
 String  receivedText = "";
@@ -11,11 +17,16 @@ String  receivedText = "";
  * | Direction       | Digital 12 | Digital 13 |             
  * | Speed (PWM)     | Digital 3  | Digital 11 |
  * | Brake           | Digital 9  | Digital 8  |
- * | Current Sensing | Analog 0 ` | Analog 1   |
+ * | Current Sensing | Analog 0 ` | Analog 1   | not necessary for basic use
  * |-----------------|------------|------------|
  * https://www.instructables.com/Arduino-Motor-Shield-Tutorial/
- * wit = 0 = !
- * zwart = 1 = _
+
+  *---------------------------------------------------*
+
+// Creates an instance
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+AccelStepper myStepper(MotorInterfaceType, 7, 5, 6, 4);
+
  */
 
 
@@ -28,15 +39,20 @@ Speed=snelheid pin
 int pinMotorLeft = 13;
 int pinBrakeLeft = 8;
 int pinSpeedLeft = 11;
-#define pinCurrentSenseLeft = A1
+
 
 int pinMotorRight = 12;
 int pinBrakeRight = 9;
 int pinSpeedRight = 3;
-#define pinCurrentSenseRight = A0
+
 int leftMotorArray[3] = {pinMotorLeft, pinBrakeLeft, pinSpeedLeft};
 int rightMotorArray[3] = {pinMotorRight, pinBrakeRight, pinSpeedRight};
 int speedMotor = 200;
+
+// Creates an instance
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+AccelStepper myStepper(MotorInterfaceType, 7, 5, 6, 4);
+int directionFork = 1;
 
 //Ultrasonic sensor
 #define echoPin 37 // attach pin D12 Arduino to pin Echo of HC-SR04
@@ -61,13 +77,13 @@ void forward() {
   analogWrite(leftMotorArray[2], speedMotor);   //Spins the motor on Channel A at full speed
 
   //Motor B backward @ half speed
-  digitalWrite(rightMotorArray[0], LOW);  //Establishes forward direction of Channel B
+  digitalWrite(rightMotorArray[0], HIGH);  //Establishes forward direction of Channel B
   digitalWrite(rightMotorArray[1], LOW);   //Disengage the Brake for Channel B
   analogWrite(rightMotorArray[2], speedMotor);    //Spins the motor on Channel B at half speed
 }
 
 void still() {
-  digitalWrite(leftMotorArray[0], HIGH); //Establishes forward direction of Channel A
+  digitalWrite(leftMotorArray[0], LOW); //Establishes forward direction of Channel A
   digitalWrite(leftMotorArray[1], HIGH);   //Disengage the Brake for Channel A
   analogWrite(leftMotorArray[2], 0);   //Spins the motor on Channel A at full speed
 
@@ -79,7 +95,7 @@ void still() {
 
 void right() {
   
-  digitalWrite(leftMotorArray[0], HIGH); //Establishes forward direction of Channel A
+  digitalWrite(leftMotorArray[0], LOW); //Establishes forward direction of Channel A
   digitalWrite(leftMotorArray[1], LOW);   //Disengage the Brake for Channel A
   analogWrite(leftMotorArray[2], 150);   //Spins the motor on Channel A at full speed
 
@@ -89,8 +105,8 @@ void right() {
   analogWrite(rightMotorArray[2], 130);    //Spins the motor on Channel B at half speed
 }
 
-void left() {
-  digitalWrite(leftMotorArray[0], LOW); //Establishes forward direction of Channel A
+void right() {
+  digitalWrite(leftMotorArray[0], HIGH); //Establishes forward direction of Channel A
   digitalWrite(leftMotorArray[1], LOW);   //Disengage the Brake for Channel A
   analogWrite(leftMotorArray[2], 120);//speedMotorTurn+40);   //Spins the motor on Channel A at full speed
 
@@ -123,16 +139,24 @@ double Sonic_Sensor() {
 void setup() {
   Serial.begin(9600); //for serial monitor
   BTSerial.begin(9600); //hc-05 default baud rate
-  pinMode(7, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT 
+  
   for(int i = 0; i < 3; i++) {
     pinMode(leftMotorArray[i], OUTPUT);
     pinMode(rightMotorArray[i], OUTPUT);
   }
+  
   forward();
   delay(1000);
   still();
+
+  myStepper.setMaxSpeed(1000.0);
+	myStepper.setAcceleration(50.0);
+	myStepper.setSpeed(200);
+	myStepper.moveTo(2038);
+
   Serial.println("vfev");
 }
   
@@ -145,35 +169,43 @@ void loop() {
     
     if(character == '\n'){
       receivedText.trim();
-      Serial.print("received: ");
-      Serial.println(receivedText);
+      Serial.print("Final string: >");
+      Serial.print(receivedText);
+      Serial.println("<");
+      if (receivedText=="stop"){
+        still();
       if (receivedText=="start"){
         
       }
       if (receivedText=="left"){
         left();
-        digitalWrite(7,HIGH);
+        digitalWrite(10,HIGH);
         
       }
       if (receivedText=="right"){
         right();
-        digitalWrite(7,HIGH);
+        digitalWrite(10,HIGH);
       }
       if (receivedText=="forward"){
         forward();
-        digitalWrite(7,HIGH);
+        digitalWrite(10,HIGH);
       }
       if (receivedText=="backwards"){
         reverse();
-        digitalWrite(7,HIGH);
+        digitalWrite(10,HIGH);
       }
       if (receivedText=="look"){
-        digitalWrite(7,HIGH);
+        digitalWrite(10,HIGH);
         
       }
-      if (receivedText=="stop"){
-        still();
+      
         
+      }
+      if (receivedText=="fork"){
+        myStepper.move(4324*directionFork);//in steps
+        directionFork *= -1;
+      
+      	
       }
       receivedText = "";
     }
---
+++ voskV3.py
@@ -0,0 +1,123 @@
+import json
+import queue
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer
+#import vosk
+from scipy.signal import butter, lfilter
+import numpy as np
+import serial
+import time
+
+#change 'COM5' to your HC-05 port(e.g., '/dev/rfcomm0' linux maxos)
+#device manager -> ports(com&LPT) -> look for the port
+btSerial = serial.Serial('COM5', baudrate=9600)#change to COM5 or COM6 if not working
+time.sleep(2)
+# Pad naar het gedownloade model (pas dit aan naar jouw locatie)
+MODEL_PATH = "languages\\vosk-model-small-en-us-0.15"
+
+# Laad het Vosk-model
+model = Model(MODEL_PATH)
+
+# Queue voor audiostream
+audio_queue = queue.Queue()
+noise_fft_profile = None
+
+SAMPLERATE = 16000
+BLOCKSIZE = 32000  # Smaller for real-time chunks
+text = ""
+
+
+def estimate_noise(audio_fft, noise_fft):
+    noise_magnitude = np.abs(noise_fft)
+    audio_magnitude = np.abs(audio_fft)
+    clean_magnitude = np.maximum(audio_magnitude - noise_magnitude, 0)
+    phase = np.angle(audio_fft)
+    return clean_magnitude * np.exp(1j * phase)
+
+def record_noise_profile(seconds=3):
+    print("Opname van ruisprofiel...")
+    noise = sd.rec(int(seconds * SAMPLERATE), samplerate=SAMPLERATE, channels=1, dtype='int16')
+    sd.wait()
+    noise = noise.flatten()
+    noise_fft = np.fft.fft(noise, n=BLOCKSIZE)
+    return noise_fft
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status)
+    chunk = np.frombuffer(indata, dtype=np.int16)
+    chunk_fft = np.fft.fft(chunk, n=BLOCKSIZE)
+    filtered_fft = estimate_noise(chunk_fft, noise_fft_profile)
+    filtered_chunk = np.fft.ifft(filtered_fft).real.astype(np.int16)
+    audio_queue.put(filtered_chunk.tobytes())
+
+
+def sendMessageToArduino(command):
+    btSerial.write((command+ "\n").encode())
+    print("sent: ", command)
+    text = ""
+
+# === Start Script ===
+noise_fft_profile = record_noise_profile()
+
+recognizer = KaldiRecognizer(model, 16000)
+recognizer.SetWords(False)  # Voorkomt volledige transcripties
+recognizer.SetGrammar('["left", "right", "start", "stop", "forward", "fork", "[unk]"]')
+lastWord = ""
+# Start audiostream
+with sd.RawInputStream(samplerate=16000, blocksize=32000, dtype='int16',
+                       channels=1, callback=callback):
+    print("Luisteren naar commando's... (Zeg 'stop' om te stoppen)")
+    while True:
+        data = audio_queue.get()
+
+        recognizer.AcceptWaveform(data)
+        result = json.loads(recognizer.PartialResult())
+        text = result.get("partial", "")#of text = result.get("text", "").strip()
+        if text:
+            #lastWord = text.split()[-1]
+            print(f"Herkenning: {text}")#full text
+            text = text.replace("[unk]", "")#unk removed
+            text = ' '.join(text.split())#spaces removed
+            print(f"Herkenning: {text}")#only commands shown
+            words = text.split()
+            
+
+            if words:
+             text = words[-1]
+            else:
+                text = ""
+
+            print("Only last word:", text)
+        print("new text:", text)
+        # Controleer op specifieke commando's
+        if "start" in text:
+            print("Commando: START gedetecteerd!")
+            sendMessageToArduino("start")
+
+            #function()moveRobot
+        if "stop"  in text:
+            print("Commando: STOP gedetecteerd! Programma beëindigen...")
+            sendMessageToArduino("stop")
+            print("Commando: STOP gedetecteerd! Programma beëindigen...")
+            #function()moveRobot
+            
+        if "left"  in text:
+            print("Commando: LINKS gedetecteerd!")
+            sendMessageToArduino("left")
+            print("Commando: LINKS gedetecteerd!")
+            #function()moveRobot
+        if "right"  in text:
+            print("Commando: RECHTS gedetecteerd!")
+            sendMessageToArduino("right")
+            print("Commando: RECHTS gedetecteerd!")
+            #function()moveRobot
+        if "forward"  in text:
+            print("Commando: FORWARD gedetecteerd!")
+            sendMessageToArduino("forward")
+            print("Commando: FORWARD gedetecteerd!")
+            #function()moveRobot
+
+            #function()moveRobot
+        #while not audio_queue.empty():
+        #    audio_queue.get()
